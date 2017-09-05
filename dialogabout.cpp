48db881755c1960b37b7bd9967348df8edb51913
Thibault <thibault@gipilab.org>
2017-09-05 16:19:20 +0200

    Switch to opensource CeCill license, remove activation keys

--- dialogabout.cpp
+++ dialogabout.cpp
@@ -1,5 +1,44 @@
+/*
+* ProtonClassicSuite
+* 
+* Copyright Thibault Mondary, Laboratoire de Recherche pour le Développement Local (2006--)
+* 
+* labo@gipilab.org
+* 
+* Ce logiciel est un programme informatique servant à cerner l'ensemble des données budgétaires
+* de la collectivité territoriale (audit, reporting infra-annuel, prévision des dépenses à venir)
+* 
+* 
+* Ce logiciel est régi par la licence CeCILL soumise au droit français et
+* respectant les principes de diffusion des logiciels libres. Vous pouvez
+* utiliser, modifier et/ou redistribuer ce programme sous les conditions
+* de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA 
+* sur le site "http://www.cecill.info".
+* 
+* En contrepartie de l'accessibilité au code source et des droits de copie,
+* de modification et de redistribution accordés par cette licence, il n'est
+* offert aux utilisateurs qu'une garantie limitée. Pour les mêmes raisons,
+* seule une responsabilité restreinte pèse sur l'auteur du programme, le
+* titulaire des droits patrimoniaux et les concédants successifs.
+* 
+* A cet égard l'attention de l'utilisateur est attirée sur les risques
+* associés au chargement, à l'utilisation, à la modification et/ou au
+* développement et à la reproduction du logiciel par l'utilisateur étant 
+* donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+* manipuler et qui le réserve donc à des développeurs et des professionnels
+* avertis possédant des connaissances informatiques approfondies. Les
+* utilisateurs sont donc invités à charger et tester l'adéquation du
+* logiciel à leurs besoins dans des conditions permettant d'assurer la
+* sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+* à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+* 
+* Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+* pris connaissance de la licence CeCILL, et que vous en avez accepté les
+* termes.
+*
+*/
+
 #include "dialogabout.h"
-#include "productactivation.h"
 #include "ui_dialogabout.h"
 #include <QDate>
 #include <QFile>
@@ -22,10 +61,7 @@ DialogAbout::DialogAbout(QWidget *parent) :
     in.setCodec("UTF-8");
     QString line=in.readAll();
     eulaFile.close();
-    ui->plainTextEdit->setPlainText(line);
-
-    ProductActivation::AvailableModules modulesFlags=product.getAvailablesModules();
-    ui->labelLicenceMode->setText(product.availableModulesToString(modulesFlags));
+    ui->plainTextEdit->setPlainText(line);    
 
 }
 
@@ -39,18 +75,3 @@ void DialogAbout::on_pushButton_clicked()
     reject();
 }
 
-void DialogAbout::on_pushButtonActivateKey_clicked()
-{
-    if(!product.askForActivationKey())
-    {
-        QMessageBox::critical(this,tr("Attention"),tr("Clé invalide"));
-    }
-    else
-    {
-        ProductActivation::AvailableModules modulesFlags=product.getAvailablesModules();
-
-        ui->labelLicenceMode->setText(product.availableModulesToString(modulesFlags));
-        accept();
-    }
-
-}
--- pcx_stringdistance.cpp
+++ pcx_stringdistance.cpp
@@ -1,3 +1,43 @@
+/*
+* ProtonClassicSuite
+* 
+* Copyright Thibault Mondary, Laboratoire de Recherche pour le Développement Local (2006--)
+* 
+* labo@gipilab.org
+* 
+* Ce logiciel est un programme informatique servant à cerner l'ensemble des données budgétaires
+* de la collectivité territoriale (audit, reporting infra-annuel, prévision des dépenses à venir)
+* 
+* 
+* Ce logiciel est régi par la licence CeCILL soumise au droit français et
+* respectant les principes de diffusion des logiciels libres. Vous pouvez
+* utiliser, modifier et/ou redistribuer ce programme sous les conditions
+* de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA 
+* sur le site "http://www.cecill.info".
+* 
+* En contrepartie de l'accessibilité au code source et des droits de copie,
+* de modification et de redistribution accordés par cette licence, il n'est
+* offert aux utilisateurs qu'une garantie limitée. Pour les mêmes raisons,
+* seule une responsabilité restreinte pèse sur l'auteur du programme, le
+* titulaire des droits patrimoniaux et les concédants successifs.
+* 
+* A cet égard l'attention de l'utilisateur est attirée sur les risques
+* associés au chargement, à l'utilisation, à la modification et/ou au
+* développement et à la reproduction du logiciel par l'utilisateur étant 
+* donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+* manipuler et qui le réserve donc à des développeurs et des professionnels
+* avertis possédant des connaissances informatiques approfondies. Les
+* utilisateurs sont donc invités à charger et tester l'adéquation du
+* logiciel à leurs besoins dans des conditions permettant d'assurer la
+* sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+* à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+* 
+* Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+* pris connaissance de la licence CeCILL, et que vous en avez accepté les
+* termes.
+*
+*/
+
 #include "pcx_stringdistance.h"
 
 PCx_StringDistance::PCx_StringDistance(const QString &str1, const QString &str2, SIMILARITYMETRIC similarityMode):str1(str1),str2(str2),metric(similarityMode)
