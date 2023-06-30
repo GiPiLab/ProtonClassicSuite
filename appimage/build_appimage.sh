691e60966d85b28ffa6492e78782b1b65c7954eb
Thibault <ThibaultMndr@users.noreply.github.com>
2023-06-30 16:04:52 +0200

    Appimage appdir now includes Graphviz plugins

---
+++ appimage/build_appimage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+linuxdeployqt-continuous-x86_64.AppImage appimage/usr/share/applications/ProtonClassicSuite.desktop -executable=appimage/usr/lib/graphviz/libgvplugin_core.so -executable=appimage/usr/lib/graphviz/libgvplugin_dot_layout.so  -executable=appimage/usr/lib/graphviz/libgvplugin_gd.so -executable=appimage/usr/lib/graphviz/libgvplugin_pango.so -executable=appimage/usr/lib/graphviz/libgvplugin_rsvg.so  -appimage
