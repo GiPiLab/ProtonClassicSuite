16f156c55e0cb63264ad629f3d3b5714f693e6b8
Thibault <ThibaultMndr@users.noreply.github.com>
2026-07-01 16:12:10 +0000

    Update appimage build file and graphviz libs

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-linuxdeployqt-continuous-x86_64.AppImage appimage/usr/share/applications/ProtonClassicSuite.desktop -executable=appimage/usr/lib/graphviz/libgvplugin_core.so -executable=appimage/usr/lib/graphviz/libgvplugin_dot_layout.so  -executable=appimage/usr/lib/graphviz/libgvplugin_gd.so -executable=appimage/usr/lib/graphviz/libgvplugin_pango.so -executable=appimage/usr/lib/graphviz/libgvplugin_rsvg.so  -appimage
+~/progs/linuxdeployqt-continuous-x86_64.AppImage appimage/usr/share/applications/ProtonClassicSuite.desktop -extra-plugins=iconengines,platforms,platformthemes,wayland-decoration-client,wayland-graphics-integration-client,wayland-shell-integration -executable=appimage/usr/lib/graphviz/libgvplugin_core.so -executable=appimage/usr/lib/graphviz/libgvplugin_dot_layout.so  -executable=appimage/usr/lib/graphviz/libgvplugin_gd.so -executable=appimage/usr/lib/graphviz/libgvplugin_pango.so -executable=appimage/usr/lib/graphviz/libgvplugin_rsvg.so  -appimage
