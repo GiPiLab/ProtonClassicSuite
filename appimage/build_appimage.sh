b23f2e0b850afb7184ac59a5b289025624922d2c
Thibault <ThibaultMndr@users.noreply.github.com>
2026-07-03 14:45:25 +0200

    Update appimage build script

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-~/progs/linuxdeployqt-continuous-x86_64.AppImage appimage/usr/share/applications/ProtonClassicSuite.desktop -extra-plugins=iconengines,platforms,platformthemes,wayland-decoration-client,wayland-graphics-integration-client,wayland-shell-integration -executable=appimage/usr/lib/graphviz/libgvplugin_core.so -executable=appimage/usr/lib/graphviz/libgvplugin_dot_layout.so  -executable=appimage/usr/lib/graphviz/libgvplugin_gd.so -executable=appimage/usr/lib/graphviz/libgvplugin_pango.so -executable=appimage/usr/lib/graphviz/libgvplugin_rsvg.so  -appimage
+#Build ProtonClassicSuite appimage with linuxdeployqt. Put ProtonClassicSuite executable in $APPIMAGE_FOLDER/usr/bin before
+
+
+LINUXDEPLOYQT="$HOME/progs/linuxdeployqt-continuous-x86_64.AppImage"
+APPIMAGE_FOLDER="appimage"
+GRAPHVIZPATH=/usr/lib/x86_64-linux-gnu/graphviz
+
+#Update graphviz from system libs
+
+cp -fv $GRAPHVIZPATH/libgvplugin_core.so.6.0.0 $GRAPHVIZPATH/libgvplugin_dot_layout.so.6.0.0 $GRAPHVIZPATH/libgvplugin_gd.so.6.0.0 $GRAPHVIZPATH/libgvplugin_pango.so.6.0.0 $GRAPHVIZPATH/libgvplugin_rsvg.so.6.0.0 $APPIMAGE_FOLDER/usr/lib/graphviz
+
+#Build appimage
+$LINUXDEPLOYQT $APPIMAGE_FOLDER/usr/share/applications/ProtonClassicSuite.desktop -extra-plugins=iconengines,platforms,platformthemes,wayland-decoration-client,wayland-graphics-integration-client,wayland-shell-integration -executable=$APPIMAGE_FOLDER/usr/lib/graphviz/libgvplugin_core.so -executable=$APPIMAGE_FOLDER/usr/lib/graphviz/libgvplugin_dot_layout.so  -executable=$APPIMAGE_FOLDER/usr/lib/graphviz/libgvplugin_gd.so -executable=$APPIMAGE_FOLDER/usr/lib/graphviz/libgvplugin_pango.so -executable=$APPIMAGE_FOLDER/usr/lib/graphviz/libgvplugin_rsvg.so  -appimage
